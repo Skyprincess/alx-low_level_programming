5b665b8e3ebf56ebde11da0aba476307ff7a99e5
Skyprincess <iyaseamaka@gmail.com>
2022-10-18 08:53:47 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main - begining here
+ * Return: Always 0
+ */
+int main(void)
+{
+	__putchar('_');
+	__putchar('p');
+	__putchar('u');
+	__putchar('t');
+	__putchar('c');
+	__putchar('h');
+	__putchar('a');
+	__putchar('r');
+	__putchar('\n');
+	return (0);
+}
