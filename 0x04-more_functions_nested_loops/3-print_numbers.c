d857a3c19ba89ae4ffac0aef5efe8d032005ae0e
Skyprincess <iyaseamaka@gmail.com>
2022-10-21 15:35:17 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0-9
+ * print_numbers - print numbers from 0 to 9
  * Return: void
  */
 
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
