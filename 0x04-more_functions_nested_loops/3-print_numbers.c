652debac7e29c12532237548349605082927e5be
Skyprincess <iyaseamaka@gmail.com>
2022-10-22 08:44:15 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,35 @@
 #include "main.h"
 
+
+
 /**
- * print_numbers - print numbers from 0 to 9
- * Return: void
- */
+ *
+ * *print_numbers - print 0123456789
+ *
+ * * owned by Bwave ICT
+ *
+ * *Return: void
+ *
+ * */
+
+
 
 void print_numbers(void)
-{
-	char c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+{
+
+		char c;
+
+
+
+			for (c = '0'; c <= '9'; c++)
+
+					{
+
+							_putchar(c);
+
+								}
+
+				_putchar('\n');
+
 }
