45995431a1346ce6aba01eaa387cee899d9d5295
Skyprincess <iyaseamaka@gmail.com>
2022-10-22 08:59:04 -0700

    5-more_number.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,45 @@
 #include "main.h"
 
+
+
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * Return: Always 0
- */
+ *
+ *  * more_numbers - nmbr fct
+ *
+ *   * Bwave ICT file
+ *
+ *    * Return: no return
+ *
+ *     */
+
+
 
 void more_numbers(void)
+
 {
+
+
 
 	int i, j;
 
-	for (i = 1, i <= 10; j++)
-	{
-		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar(j % 10 + '0');
-		}
-		_putchar('\n');
-	}
+		for (i = 1; i <= 10; i++)
+
+				{
+
+							for (j = 0; j <= 14; j++)
+
+										{
+
+														if (j >= 10)
+
+																		_putchar('1');
+
+																	_putchar(j % 10 + '0');
+
+																			}
+
+										_putchar('\n');
+
+												}
+
 }
